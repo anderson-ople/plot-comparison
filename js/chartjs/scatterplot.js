489b810fd381ab3715d11cc82043d3f6ab878a0b
Juan Mejia <juanm@ople.ai>
2019-04-08 17:07:31 -0500

    Moved Chartjs code from demo.html to a js file

---
+++ js/chartjs/scatterplot.js
@@ -0,0 +1,73 @@
+var color = Chart.helpers.color;
+var data_setosa = [[3.5, 3.0, 3.2, 3.1, 3.6, 3.9, 3.4, 3.4, 2.9, 3.1, 3.7, 3.4, 3.0, 3.0, 4.0, 4.4, 3.9, 3.5, 3.8, 3.8, 3.4, 3.7, 3.6, 3.3, 3.4, 3.0, 3.4, 3.5, 3.4, 3.2, 3.1, 3.4, 4.1, 4.2, 3.1, 3.2, 3.5, 3.6, 3.0, 3.4, 3.5, 2.3, 3.2, 3.5, 3.8, 3.0, 3.8, 3.2, 3.7, 3.3],[0.2, 0.2, 0.2, 0.2, 0.2, 0.4, 0.3, 0.2, 0.2, 0.1, 0.2, 0.2, 0.1, 0.1, 0.2, 0.4, 0.4, 0.3, 0.3, 0.3, 0.2, 0.4, 0.2, 0.5, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.2, 0.4, 0.1, 0.2, 0.2, 0.2, 0.2, 0.1, 0.2, 0.2, 0.3, 0.3, 0.2, 0.6, 0.4, 0.3, 0.2, 0.2, 0.2, 0.2]]
+var data_versicolor =[[3.2, 3.2, 3.1, 2.3, 2.8, 2.8, 3.3, 2.4, 2.9, 2.7, 2.0, 3.0, 2.2, 2.9, 2.9, 3.1, 3.0, 2.7, 2.2, 2.5, 3.2, 2.8, 2.5, 2.8, 2.9, 3.0, 2.8, 3.0, 2.9, 2.6, 2.4, 2.4, 2.7, 2.7, 3.0, 3.4, 3.1, 2.3, 3.0, 2.5, 2.6, 3.0, 2.6, 2.3, 2.7, 3.0, 2.9, 2.9, 2.5, 2.8],[1.4, 1.5, 1.5, 1.3, 1.5, 1.3, 1.6, 1.0, 1.3, 1.4, 1.0, 1.5, 1.0, 1.4, 1.3, 1.4, 1.5, 1.0, 1.5, 1.1, 1.8, 1.3, 1.5, 1.2, 1.3, 1.4, 1.4, 1.7, 1.5, 1.0, 1.1, 1.0, 1.2, 1.6, 1.5, 1.6, 1.5, 1.3, 1.3, 1.3, 1.2, 1.4, 1.2, 1.0, 1.3, 1.2, 1.3, 1.3, 1.1, 1.3]]
+var data_virginica = [[3.3, 2.7, 3.0, 2.9, 3.0, 3.0, 2.5, 2.9, 2.5, 3.6, 3.2, 2.7, 3.0, 2.5, 2.8, 3.2, 3.0, 3.8, 2.6, 2.2, 3.2, 2.8, 2.8, 2.7, 3.3, 3.2, 2.8, 3.0, 2.8, 3.0, 2.8, 3.8, 2.8, 2.8, 2.6, 3.0, 3.4, 3.1, 3.0, 3.1, 3.1, 3.1, 2.7, 3.2, 3.3, 3.0, 2.5, 3.0, 3.4, 3.0],[2.5, 1.9, 2.1, 1.8, 2.2, 2.1, 1.7, 1.8, 1.8, 2.5, 2.0, 1.9, 2.1, 2.0, 2.4, 2.3, 1.8, 2.2, 2.3, 1.5, 2.3, 2.0, 2.0, 1.8, 2.1, 1.8, 1.8, 1.8, 2.1, 1.6, 1.9, 2.0, 2.2, 1.5, 1.4, 2.3, 2.4, 1.8, 1.8, 2.1, 2.4, 2.3, 1.9, 2.3, 2.5, 2.3, 1.9, 2.0, 2.3, 1.8]]
+function generateData(data_array) {
+    var data = []
+    for (var i = 0; i < 50; i++) {
+	data.push({
+	    x: data_array[0][i],
+	    y: data_array[1][i]
+	});
+    }
+    console.log(data);
+    return data;
+}
+
+var scatterChartData = {
+    datasets: [{
+	label: 'Setosa dataset',
+	borderColor: window.chartColors.blue,
+	backgroundColor: color(window.chartColors.blue).alpha(0.6).rgbString(),
+	data: generateData(data_setosa)
+    }, {
+	label: 'Versicolor dataset',
+	borderColor: window.chartColors.red,
+	backgroundColor: color(window.chartColors.red).alpha(0.6).rgbString(),
+	data: generateData(data_versicolor)
+    }, {
+	label: 'Virginica dataset',
+	borderColor: window.chartColors.green,
+	backgroundColor: color(window.chartColors.green).alpha(0.6).rgbString(),
+	data: generateData(data_virginica)
+    }]
+};
+
+window.onload = function() {
+    var ctx = document.getElementById('canvas').getContext('2d');
+    window.myScatter = Chart.Scatter(ctx, {
+	data: scatterChartData,
+	options: {
+	    title: {
+		display: true,
+		text: 'Chart.js Scatter Chart'
+	    },
+	    plugins:{
+		zoom: {
+		    pan: {
+			enabled: true,
+			mode: 'xy',
+			speed: 10,
+			threshold: 10
+		    },
+		    zoom: {
+			enabled: true,
+			mode: 'xy'
+		    }  
+		}
+	    }
+	}
+    });
+};
+
+document.getElementById('randomizeData').addEventListener('click', function() {
+    scatterChartData.datasets.forEach(function(dataset) {
+	dataset.data = dataset.data.map(function() {
+	    return {
+		x: randomScalingFactor(),
+		y: randomScalingFactor()
+	    };
+	});
+    });
+    window.myScatter.update();
+});
